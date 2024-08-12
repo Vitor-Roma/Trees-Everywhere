cd8ea3b8c28051581fde92dfc8f5bd33b2befa04
Vitor <vitorvxroma@hotmail.com>
2024-08-12 18:46:37 -0300

    humanize age

--- trees/models.py
+++ trees/models.py
@@ -1,8 +1,9 @@
-import datetime
+from django.utils import timezone
 from dataclasses import dataclass
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.exceptions import ValidationError
+import humanize
 
 
 @dataclass
@@ -52,7 +53,7 @@ class PlantedTree(BaseModel):
 
     @property
     def age(self) -> str:
-        return f"{self.tree.created_at.year - datetime.datetime.now().year} anos"
+        return humanize.naturaltime(timezone.now() - self.tree.created_at)
 
 
 class Account(BaseModel):
